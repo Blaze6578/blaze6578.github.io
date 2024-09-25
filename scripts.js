a72f921695ff5818bb8314270a87bc992c6fd5c1
Blaze6578 <107383944+Blaze6578@users.noreply.github.com>
2024-09-25 11:39:11 +1000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('createRoomBtn').addEventListener('click', function() {
+    alert('Create Room button clicked!');
+});
+
+document.getElementById('joinRoomBtn').addEventListener('click', function() {
+    alert('Join Room button clicked!');
+});
